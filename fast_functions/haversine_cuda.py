efa2cb9390d118d375644bff8c46d146cf9ed9cf
Ryan Culligan <rrculligan@gmail.com>
2020-03-29 09:45:00 -0500

    updates

---
+++ fast_functions/haversine_cuda.py
@@ -0,0 +1,31 @@
+import cupy as cp
+
+MeterConverter = dict(
+    kilometers = 1.0,
+    meters = 1000.0,
+    miles = 0.621371192,
+    nautical_miles = 0.539956803,
+    feet = 3280.839895013,
+    inches = 39370.078740158,
+)
+
+
+def haversine_array_cuda(
+    lat1: cp.array, 
+    lng1: cp.array, 
+    lat2: cp.array,
+    lng2: cp.array, 
+    unit: float=MeterConverter["kilometers"]
+) -> cp.array:
+    rad_lat1 = cp.radians(lat1)
+    rad_lng1 = cp.radians(lng1)
+    rad_lat2 = cp.radians(lat2)
+    rad_lng2 = cp.radians(lng2)
+    lat_delta = rad_lat2 - rad_lat1
+    lng_delta = rad_lng2 - rad_lng1
+    a = cp.sin(lat_delta / 2) ** 2 + cp.cos(rad_lat1) * cp.cos(rad_lat2) * cp.sin(lng_delta / 2) ** 2
+    c = 2 * cp.arcsin(cp.sqrt(a))
+    km = 6371 * c
+    converted = unit * km
+    return converted
+
--- tests/test_haversine_cuda.py
+++ tests/test_haversine_cuda.py
@@ -1,16 +1,16 @@
 try:
 
-    import numpy as np
+    import cupy as cp
     from fast_functions.haversine_cuda import haversine_array_cuda
 
     avon_lat, avon_lng = 45.6091, -94.4517
     minneapolis_lat, minneapolis_lng = 44.9778, -93.2650
 
     def test_haversine_array_cuda():
-        lat1 = np.array([avon_lat] * 1_000)
-        lng1 = np.array([avon_lng] * 1_000)
-        lat2 = np.array([minneapolis_lat] * 1_000)
-        lng2 = np.array([minneapolis_lng] * 1_000)
+        lat1 = cp.array([avon_lat] * 1_000)
+        lng1 = cp.array([avon_lng] * 1_000)
+        lat2 = cp.array([minneapolis_lat] * 1_000)
+        lng2 = cp.array([minneapolis_lng] * 1_000)
         results = haversine_array_cuda(lat1, lng1, lat2, lng2)
         assert all([round(result, 2) == 116.38 for result in results])
 
